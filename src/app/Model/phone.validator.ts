fd05f6060de136d3608b488f54cf15fa1c60c64d
Andrey Pahomov <andrey.pahomov@gmail.com>
2017-12-27 02:51:32 +0300

    removed custom validators to Model catalog

--- src/app/Model/phone.validator.ts
+++ src/app/Model/phone.validator.ts
@@ -2,10 +2,7 @@ import { FormControl } from '@angular/forms';
 
 
 export function validatePhone(c: FormControl) {
-	let PHONE_REGEXP = /^\+\d+(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{2}(-|\s)\d{2}$/;
-
-	console.log(PHONE_REGEXP);
-	console.log(c);
+	let PHONE_REGEXP = /^\+\d+\s?(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{2}(-|\s)\d{2}$/;
 
 	return PHONE_REGEXP.test(c.value) ? null : {
 		validatePhone: {
