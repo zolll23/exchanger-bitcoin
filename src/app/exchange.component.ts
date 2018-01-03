4d0501474a452d6e72268374cf036b61754aa63c
Andrey Pahomov <andrey.pahomov@gmail.com>
2018-01-03 20:06:57 +0300

    support auto refresh rates (per 1 minute) of crypto coins and removed button «refresh» after list of rates

--- src/app/exchange.component.ts
+++ src/app/exchange.component.ts
@@ -33,7 +33,7 @@ export class ExchangeComponent implements AfterViewInit {
 
     ngOnInit() {
         this.timer_id = setInterval(() => {
-            //this.refreshCurrencies(); 
+            this.refreshCurrencies(); 
         }, 60000);
     }
 
