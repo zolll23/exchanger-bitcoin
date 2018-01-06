71aaad68b9409d6a667e32a2bd9b8ffe82bd336d
Andrey Pahomov <andrey.pahomov@gmail.com>
2018-01-06 17:13:19 +0300

    fixed bug with set values as null of rates after fill other fields

--- src/app/exchange.module.ts
+++ src/app/exchange.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from "@angular/http";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from "@angular/router";
 
@@ -8,6 +9,8 @@ import { RateDataSource } from './Model/rate.datasource';
 import { RateRepository } from "./Model/rate.repository";
 import { InfoDataSource } from './Model/info.datasource';
 import { InfoRepository } from "./Model/info.repository";
+import { RestDataSource } from "./Model/rest.datasource";
+import { OrderRepository } from "./Model/order.repository";
 
 import { ExchangeComponent } from "./exchange.component";
 import { InfoComponent } from "./info.component";
@@ -15,7 +18,7 @@ import { InfoComponent } from "./info.component";
 @NgModule({
     imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, RouterModule],
     declarations: [ExchangeComponent, InfoComponent],
-    providers: [RateRepository, RateDataSource, InfoRepository, InfoDataSource],
+    providers: [RateRepository, RateDataSource, InfoRepository, InfoDataSource, RestDataSource, OrderRepository ],
     exports: [ExchangeComponent,InfoComponent]
 })
 export class ExchangeModule { }
