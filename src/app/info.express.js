8214cab355c6f66ab53cf176c1b456bae7dce550
Andrey Pahomov <andrey.pahomov@gmail.com>
2018-01-06 22:45:34 +0300

    support on server-side saving form data in MongoDB

--- src/app/info.express.js
+++ src/app/info.express.js
@@ -1,9 +1,40 @@
 //info.express.js
 
-var express = require("express");
-var fs = require("fs");
+const express = require("express");
+const cors = require('cors');
+const body_parser = require('body-parser');
+const fs = require("fs");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/bitcoinbycard");
+
+const orderSchema = new mongoose.Schema({
+	id:String,
+	give:String,
+	amount_give:String,
+	receive:String,
+	amount_receive:String,
+	email:String,
+	phone:String,
+	date:String,
+	address:String,
+	serial:String,
+	month:String,
+	year:String,
+	code:String,
+	cardholder:String
+	});
+
+var Orders = mongoose.model("Orders", orderSchema);
 
 var app = express();
+
+app.use(cors());
+app.options('*', cors());
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended: true}));
+
 
 // Logging server requests
 app.use(function(request, response, next){
@@ -18,12 +49,6 @@ app.use(function(request, response, next){
     next();
 });
 
-app.use(function (request, response, next) {
-	response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    response.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 app.get("/public/:info", function (request, response) {
 
@@ -46,6 +71,17 @@ app.get("/public/:info", function (request, response) {
 			response.send(JSON.stringify(article));
 		}
 	});
+});
+
+app.post("/orders",function (request, response) {
+	console.log(request.body);
+	var myData = new Orders(request.body);
+	myData.save().then(item => {
+		response.send('Ok');
+	}).catch (err => {
+		response.status(400).send('Error');
+	});
+	
 });
  
 app.listen(3000);
